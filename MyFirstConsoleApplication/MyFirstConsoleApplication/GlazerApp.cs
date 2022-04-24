4884f2079612f1679b38c3a1d70df74e14851bf6
Warner-Felicia <foodleswarner@gmail.com>
2022-04-23 21:52:47 -0400

    created number validation helper function

--- MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
@@ -7,49 +7,38 @@ namespace MyFirstConsoleApplication
         public static void RunExample()
         {
             //get window length from user and convert to a double
-            double width;
-            
-            while (true)
-            {
-                Console.WriteLine("Please enter the length of the window in feet");
-                var widthString = Console.ReadLine();
-                if (!double.TryParse(widthString, out width))
-                {
-                    continue;
-                }
-
-                if (!(width > 0))
-                {
-                    continue;
-                }
-
-                break;
-            }
+            var width = GetValidInput("width");
             
             //get window height from user and convert to a double
-            double height;
-            while (true)
-            {
-                Console.WriteLine("Please enter the height of the window in feet");
-                var heightString = Console.ReadLine();
-                if (!double.TryParse(heightString, out height))
-                {
-                    continue;
-                }
-
-                if (!(height > 0))
-                {
-                    continue;
-                }
-
-                break;
-            }
+            var height = GetValidInput("height");
 
             //calculate the area for glass and perimeter for wood frame and output to console
             var woodLength = 2 * (width + height) * 3.25;
             var glassArea = 2 * (width * height);
             Console.WriteLine($"The length of the wood is {woodLength} feet.");
             Console.WriteLine($"The area of the glass is {glassArea} square meters.");
+        }
+
+        public static double GetValidInput(string dimension)
+        {
+            double number;
+            while (true)
+            {
+                Console.WriteLine($"Please enter the {dimension} of the window in feet");
+                var numberString = Console.ReadLine();
+                if (!double.TryParse(numberString, out number))
+                {
+                    continue;
+                }
+
+                if (!(number > 0))
+                {
+                    continue;
+                }
+
+                break;
+            }
+            return number;
         }
     }
 
