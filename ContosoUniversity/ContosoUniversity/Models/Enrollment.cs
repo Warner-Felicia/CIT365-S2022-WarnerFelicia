87b7fa09cb0bb067f4ba0b9c05ef0e98be44117b
Warner-Felicia <foodleswarner@gmail.com>
2022-06-21 20:53:41 -0400

    Completed create complex data model in EF Core with MVC

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,17 +1,23 @@
-﻿namespace ContosoUniversity.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace ContosoUniversity.Models
+{
     public enum Grade
     {
         A, B, C, D, F
     }
+
     public class Enrollment
     {
+        // ReSharper disable once InconsistentNaming
         public int EnrollmentID { get; set; }
+        // ReSharper disable once InconsistentNaming
         public int CourseID { get; set; }
+        // ReSharper disable once InconsistentNaming
         public int StudentID { get; set; }
-
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
+
         public Course Course { get; set; }
         public Student Student { get; set; }
     }
