23b08a221715944ba12ac80219f3bbd89f5d66a9
Warner-Felicia <foodleswarner@gmail.com>
2022-07-19 10:50:00 -0400

    added print ability

---
+++ Html2pdfAttempt/Html2pdfAttempt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Html2pdfAttempt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ITextSharpAttempt/ITextSharpAttempt/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using ITextSharpAttempt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+
+
+namespace ITextSharpAttempt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        protected void GeneratePDF(object sender, System.EventArgs e)
+        {
+            using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+            {
+                Document document = new Document(PageSize.A4, 10, 10, 10, 10);
+
+                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                document.Open();
+
+                Chunk chunk = new Chunk("This is from chunk. ");
+                document.Add(chunk);
+
+                Phrase phrase = new Phrase("This is from Phrase.");
+                document.Add(phrase);
+
+                Paragraph para = new Paragraph("This is from paragraph.");
+                document.Add(para);
+
+                string text = @"you have successfully created PDF file.";
+                Paragraph paragraph = new Paragraph();
+                paragraph.SpacingBefore = 10;
+                paragraph.SpacingAfter = 10;
+                paragraph.Alignment = Element.ALIGN_LEFT;
+                paragraph.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.GREEN);
+                paragraph.Add(text);
+                document.Add(paragraph);
+
+                document.Close();
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+                Response.Clear();
+                Response.ContentType = "application/pdf";
+
+                string pdfName = "User";
+                HttpContext.Response.AddHeader("Content-Disposition", "attachment; filename=" + pdfName + ".pdf");
+                Response.ContentType = "application/pdf";
+                Response.Buffer = true;
+                Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+                Response.BinaryWrite(bytes);
+                Response.End();
+                Response.Close();
+            }
+        }
+
+    }
+}
